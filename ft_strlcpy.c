5e9ab1442672b1d08dc65fb6a1dcc689264ee826
Takumi Mori <tamori@c1r10s1.42tokyo.jp>
2024-01-11 20:29:28 +0900

    aa

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,36 +6,26 @@
 /*   By: tamori <tamori@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 21:48:34 by tamori            #+#    #+#             */
-/*   Updated: 2024/01/09 22:33:51 by tamori           ###   ########.fr       */
+/*   Updated: 2024/01/11 20:11:17 by tamori           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *s1, const void *s2, size_t n)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	len;
+	size_t	count;
 
-	len = 0;
-	if (s2 < s1)
+	count = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[count] != '\0' && count < (size - 1))
 	{
-		len = n;
-		while (len > 0)
-		{
-			len--;
-			((unsigned char *)s1)[len] = ((unsigned char *)s2)[len];
-		}
+		dest[count] = src[count];
+		count++;
 	}
-	else
-	{
-		len = 0;
-		while (len < n)
-		{
-			((unsigned char *)s1)[len] = ((unsigned char *)s2)[len];
-			len++;
-		}
-	}
-	return (s1);
+	dest[count] = '\0';
+	return (ft_strlen(src));
 }
 
 // int main(){
